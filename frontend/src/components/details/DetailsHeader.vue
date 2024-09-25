cf2b0e930a1898f35900bc0b58c1893fc58b7047
kirchner <kirchner@visionconnect.de>
2024-09-25 18:12:15 +0200

    WiP | Added Editing to plants

---
+++ frontend/src/components/details/DetailsHeader.vue
@@ -0,0 +1,82 @@
+<template>
+  <ion-header>
+    <ion-toolbar class="header-toolbar">
+      <ion-buttons slot="start">
+        <ion-back-button text="Zurück"></ion-back-button>
+      </ion-buttons>
+      <ion-icon
+        v-if="showEditButton"
+        :icon="editIcon"
+        style="width: 32px; height: 32px"
+        slot="end"
+        @click="onEditClick"
+      />
+    </ion-toolbar>
+  </ion-header>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonIcon,
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "DetailsHeader",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonSegment,
+    IonSegmentButton,
+    IonLabel,
+    IonIcon,
+  },
+  props: {
+    showEditButton: {
+      type: Boolean,
+      default: true,
+    },
+    editIcon: {
+      type: String,
+      default: "create",
+    },
+    onEditClick: {
+      type: Function as PropType<() => void>,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      segmentValue: this.startingSegment,
+    };
+  },
+  methods: {
+    handleEditClick() {
+      if (this.onEditClick) {
+        this.onEditClick();
+      }
+    },
+  },
+});
+</script>
+
+<style scoped>
+.header-toolbar {
+  background-color: var(--ion-color-primary);
+  color: white;
+}
+
+.segment-toolbar {
+  background-color: var(--ion-color-light);
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+}
+</style>
---
+++ frontend/src/views/plants/PlantEditing.vue
@@ -0,0 +1,156 @@
+<template>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <ion-buttons slot="start">
+          <ion-back-button text="Zurück"></ion-back-button>
+        </ion-buttons>
+        <IonTitle>Pflanze editieren</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <form-component
+        :item="plant"
+        :formFields="[
+          { type: 'input', modelKey: 'name', label: 'Name', required: true },
+          {
+            type: 'input',
+            modelKey: 'species',
+            label: 'Spezies',
+            required: true,
+          },
+          {
+            type: 'select',
+            modelKey: 'substrateId',
+            label: 'Substrat',
+            placeholder: 'Substrat auswählen',
+            options: substrates,
+          },
+          {
+            type: 'radio',
+            modelKey: 'isPublic',
+            label: 'Sichtbarkeit',
+            options: [
+              { value: true, label: 'Öffentlich' },
+              { value: false, label: 'Privat' },
+            ],
+          },
+        ]"
+        cardTitle="Planzen Informationen"
+        submitLabel="Pflanze editieren"
+        :extraContentComponent="SubstrateContainer"
+        :extraContentData="{ substrate: selectedSubstrate }"
+        @submit="editPlant"
+      ></form-component>
+    </IonContent>
+  </IonPage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonButton,
+  IonRadioGroup,
+  IonRadio,
+} from "@ionic/vue";
+import FormComponent from "@/components/adding/FormComponent.vue";
+import SubstrateContainer from "@/components/plants/SubstrateContainer.vue";
+
+import PlantService from "@/services/PlantService";
+import SubstrateService from "@/services/SubstrateService";
+import ToastService from "@/services/general/ToastService";
+
+export default defineComponent({
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonSelect,
+    IonSelectOption,
+    IonButton,
+    IonRadioGroup,
+    IonRadio,
+
+    FormComponent,
+    SubstrateContainer,
+  },
+  data() {
+    return {
+      plant: {
+        name: "",
+        species: "",
+        substrateId: 0,
+        isPublic: false, // Default to private
+      } as AddPlant,
+      substrates: [] as Substrate[], // Substrate data will be fetched from API
+    };
+  },
+  setup() {
+    return { SubstrateContainer };
+  },
+  async mounted() {
+    await this.fetchSubstrates(); // Fetch substrates when component mounts
+  },
+  computed: {
+    selectedSubstrate() {
+      return this.substrates.find(
+        (substrate) => substrate.id === this.plant.substrateId
+      );
+    },
+  },
+  methods: {
+    async fetchSubstrates() {
+      try {
+        const response = await SubstrateService.getSubstrates();
+
+        this.substrates = response;
+      } catch (error) {
+        console.error("Error fetching substrates:", error);
+      }
+    },
+    async editPlant() {
+      if (!this.plant.name || !this.plant.species || !this.plant.substrateId) {
+        ToastService.showWarning("All fields are required!");
+        return;
+      }
+
+      try {
+        const response = await PlantService.updatePlant(this.plant);
+        if (response) {
+          this.$router.push("/plants"); // Redirect to plant list after success
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        ToastService.showError("Error while adding the plant");
+      }
+    },
+  },
+});
+</script>
